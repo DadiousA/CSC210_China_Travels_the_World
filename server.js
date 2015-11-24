fa093b422df27950311045686f8e0fa5a126cd01
U-GESBO\Shengbo <gesboge@gmail.com>
2015-11-24 14:36:46 -0500

    functional implemented

--- server.js
+++ server.js
@@ -17,9 +17,7 @@ var formidable = require('formidable');
 var path = require('path');
 var util = require('util');
 var fs = require('fs-extra');
-var mkdirp = require('mkdirp'); 
 var db = new sqlite3.Database('WanU.db');
-mkdirp('static_files/user_files/', function (err) {});
 
 db.run('CREATE TABLE IF NOT EXISTS WanU_user (email TEXT PRIMARY KEY, name TEXT, log_in INTEGER, language TEXT, city TEXT)');
 
@@ -30,7 +28,7 @@ app.post('/users', function (req, res) {
     if (row.length==0)
     {
       db.run("INSERT INTO WanU_user (email, name, log_in) VALUES (?,?,?)", [postBody.email,postBody.name,0]);  
-      mkdirp('static_files/user_files/'+postBody.email, function (err) {});
+      fs.mkdirpSync('static_files/user_files/'+postBody.email);
       res.send('Account created.');
       return;
     }
@@ -75,7 +73,11 @@ app.put('/user/*', function (req, res) {
 app.delete('/user/*', function (req, res) {
   var userEmail = req.params[0]; 
   db.run("DELETE FROM WanU_user WHERE email=?",[userEmail]);
+  var images=fs.readdirSync(__dirname+'/static_files/user_files/'+userEmail);
+  for(i=0;i<images.length;i++)
+    fs.removeSync(__dirname+"/static_files/image_base/"+images[i]);
   fs.removeSync(__dirname+"/static_files/user_files/"+userEmail);
+  fs.removeSync(__dirname+"/static_files/user_prompt/"+userEmail);
   res.send("OK");
 });
 
@@ -124,8 +126,39 @@ app.get('/image', function (req, res) {
   res.send({photos: files});
 });
 
+app.put('/image_desc/*', function (req, res) {
+  var userEmail = req.params[0]; 
+  var desc = req.body.description;
+  var index = req.body.index;
+  var images=fs.readdirSync(__dirname+'/static_files/user_files/'+userEmail);
+  fs.outputFileSync(__dirname+"/static_files/user_prompt/"+userEmail+"/"+images[index]+".txt",desc);
+});
 
+app.get('/image_desc/*', function (req, res) {
+  var userEmail = req.params[0].split('/')[0];
+  var userIndex = req.params[0].split('/')[1];
+  var images=fs.readdirSync(__dirname+'/static_files/user_files/'+userEmail);
+  try{
+    var desc =fs.readFileSync(__dirname+"/static_files/user_prompt/"+userEmail+"/"+images[userIndex]+".txt",'utf8');
+  }catch(err){
+    res.send();
+    return;
+  }
+  res.send(desc);
+});
 
+app.delete('/image/*', function(req,res){
+  var index = req.body.index;
+  var userEmail = req.params[0]; 
+  var images=fs.readdirSync(__dirname+'/static_files/user_files/'+userEmail);
+  fs.removeSync(__dirname+"/static_files/image_base/"+images[index]);
+  fs.removeSync(__dirname+"/static_files/user_files/"+userEmail+"/"+images[index]);
+  try{
+    fs.removeSync(__dirname+"/static_files/user_prompt/"+userEmail+"/"+images[index]+".txt");
+  }catch(err){}
+  var files=fs.readdirSync(__dirname+'/static_files/user_files/'+userEmail);
+  res.send({photos: files});
+});
 
 
 // start the server on http://localhost:3000/
